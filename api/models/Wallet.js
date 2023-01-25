7b9a892f0cbcbd5f59de80aaca275656a3ce2755
anderlfrias <anderl.friaas@gmail.com>
2023-01-25 10:09:52 -0400

    feat: Controlador Wallet creado

---
+++ api/models/Wallet.js
@@ -0,0 +1,46 @@
+/**
+ * Wallet.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    name: {
+      type: 'string',
+      required: true,
+      unique: true,
+    },
+    amount: {
+      type: 'number',
+      required: true,
+    },
+    description: {
+      type: 'string',
+      required: false,
+    },
+    user: {
+      model: 'user',
+      required: true,
+    },
+  },
+
+};
+
