49502dc042d3b6c2e884be950460d599e0857cf1
anderlfrias <anderl.friaas@gmail.com>
2023-01-25 10:09:13 -0400

    feat: Controlador User creado

---
+++ api/models/User.js
@@ -0,0 +1,67 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    name: {
+      type: 'string',
+      required: true
+    },
+    firstSurname: {
+      type: 'string',
+      required: true
+    },
+    secondSurname: {
+      type: 'string',
+    },
+    email: {
+      type: 'string',
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: 'string',
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: 'string',
+      required: true,
+    },
+    role: {
+      type: 'string',
+      isIn: ['admin', 'user'],
+      defaultsTo: 'user',
+    },
+    isActive: {
+      type: 'boolean',
+      defaultsTo: false,
+    },
+    wallets: {
+      collection: 'wallet',
+      via: 'user'
+    },
+  },
+
+};
+
