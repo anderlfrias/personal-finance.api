684aead19ae91bdceab58bfd1f7aecc9a388650d
Anderson Frias <anderl.friaas@gmail.com>
2023-02-14 19:37:32 -0400

    Modelo de presupuesto, listo

---
+++ api/models/Budget.js
@@ -0,0 +1,56 @@
+/**
+ * Budget.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    name: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string'
+    },
+    amount: {
+      type: 'number',
+      required: true,
+    },
+    startDate: {
+      type: 'string',
+      required: true,
+    },
+    endDate: {
+      type: 'string',
+      required: true,
+    },
+    user: {
+      model: 'user',
+      required: true,
+    },
+    category: {
+      model: 'category',
+      required: true,
+    },
+  },
+
+};
+
