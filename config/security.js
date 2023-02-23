84a967e0a0beda64653db9d6a96ea4e5252f92ae
anderlfrias <anderl.friaas@gmail.com>
2023-02-23 08:19:19 -0400

    Solucion del problema relacionado con los cors

--- config/security.js
+++ config/security.js
@@ -31,7 +31,9 @@ module.exports.security = {
   cors: {
     allRoutes: true,
     allowOrigins: '*',
-    allowCredentials: false
+    allowCredentials: false,
+    allowRequestMethods: 'GET, POST, PUT, DELETE',
+    allowRequestHeaders: 'Content-Type, Authorization',
   },
 
 
